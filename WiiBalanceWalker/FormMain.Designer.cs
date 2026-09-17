05e6e8a81102c1d5c5bc8663fbc3fe28ae14d7ea
hraabis <hraabis@gmail.com>
2026-09-17 16:42:19 -0600

    Update display Version in FormMain

--- WiiBalanceWalker/FormMain.Designer.cs
+++ WiiBalanceWalker/FormMain.Designer.cs
@@ -1358,7 +1358,7 @@
             this.MaximizeBox = false;
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Wii Balance Walker - Version 0.5";
+            this.Text = "Wii Balance Walker - Version 0.6";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.groupBox_RawWeight.ResumeLayout(false);
